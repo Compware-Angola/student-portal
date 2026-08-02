c3ba2388850e7882c4cbb7f038b8b6ff52afe367
jose005111 <jose29032002@hotmail.com>
2026-08-02 16:41:46 +0100

    feat: update PrePayment component to use current academic year query

--- src/pages/pre-payment/index.tsx
+++ src/pages/pre-payment/index.tsx
@@ -1,19 +1,16 @@
 import { useQueryProfile } from '@/hooks/profile/use-query-profile'
 import { PreInvoice } from './components/pre-invoice'
-import { useQueryAcademicYear } from '@/hooks/academic-year/use-query-academic-year'
+import { useQueryCurrentAcademicYear } from '@/hooks/academic-year/use-query-current-academic-year'
 
 export const PrePayment = () => {
   const { profileData } = useQueryProfile()
-  const { data: academicYears } = useQueryAcademicYear()
-
-  const activeYear = academicYears?.anolectivos?.find(
-    (year) => year.estado === 'Activo'
-  )
-
+  const {
+    data: currentAcademicYear,
+  } = useQueryCurrentAcademicYear()
   return (
     <PreInvoice
       enrollmentCode={String(profileData?.codigo_preinscricao)}
-      selectedYear={String(activeYear?.codigo ?? '')}
+      selectedYear={String(currentAcademicYear?.codigo ?? '')}
     />
   )
 }
