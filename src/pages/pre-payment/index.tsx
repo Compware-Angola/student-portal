e3d174e5c74548cebb9ba04374cc7047f339f4c4
jose005111 <jose29032002@hotmail.com>
2026-08-02 16:36:01 +0100

    feat: update PrePayment component to dynamically select active academic year

--- src/pages/pre-payment/index.tsx
+++ src/pages/pre-payment/index.tsx
@@ -1,13 +1,19 @@
 import { useQueryProfile } from '@/hooks/profile/use-query-profile'
 import { PreInvoice } from './components/pre-invoice'
+import { useQueryAcademicYear } from '@/hooks/academic-year/use-query-academic-year'
 
 export const PrePayment = () => {
   const { profileData } = useQueryProfile()
+  const { data: academicYears } = useQueryAcademicYear()
+
+  const activeYear = academicYears?.anolectivos?.find(
+    (year) => year.estado === 'Activo'
+  )
 
   return (
     <PreInvoice
       enrollmentCode={String(profileData?.codigo_preinscricao)}
-      selectedYear="244"
+      selectedYear={String(activeYear?.codigo ?? '')}
     />
   )
 }
