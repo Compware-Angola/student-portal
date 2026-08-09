59ea55bf4adff8ddd93bda1394b1a1c04c2f78b1
jose005111 <jose29032002@hotmail.com>
2026-08-09 21:55:04 +0100

    Nova tela para controle de UCs na grade

--- src/pages/pre-subscription/licenciatura/components/form-provider.tsx
+++ src/pages/pre-subscription/licenciatura/components/form-provider.tsx
@@ -114,7 +114,7 @@ export function FormPreSubscriptionProvider({
       maritalStatus: '',
       motherName: '',
       needs: '',
-      averageGrade: 0,
+      averageGrade: '',
       graduationYear: '',
       previousSchool: '',
       previousCourse: '',
